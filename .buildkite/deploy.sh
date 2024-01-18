1af91c8291f9bb4a4bf951415996cd89fcc25294
Joseph Phelan <jphelan@widen.com>
2024-01-18 09:05:34 -0600

    test

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -3,13 +3,14 @@ set -euo pipefail
 
 # Get the tag version from Buildkite or default to a pre-release tag
 TAG_VERSION=$(buildkite-agent meta-data get "tag-version" --default "0.0.0-alpha-$(date +%Y%m%d%H%M%S)")
-TAG_VERSION=$(TAG_VERSION/v/)
+TAG_VERSION=${TAG_VERSION/v/}
 
 #setup git
 # git config user.email "Widen Developers"
 # git config user.name "widendev@widen.com"
 
 # Publish the package
+echo "publishing ${TAG_VERSION}"
 npm publish --tag ${TAG_VERSION}
 npm version ${TAG_VERSION} --no-git-tag-version
 
