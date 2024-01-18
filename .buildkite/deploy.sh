afe05480aea3da9a51e7b6e2c8f84bc3392925c9
Joseph Phelan <jphelan@widen.com>
2024-01-18 10:20:02 -0600

    cleanup version snafoo

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 # Get the tag version from Buildkite or default to a pre-release tag
 TAG_VERSION=$(buildkite-agent meta-data get "tag-version" --default "0.0.0-alpha-$(date +%Y%m%d%H%M%S)")
-TAG_VERSION=${TAG_VERSION/v/}
+# TAG_VERSION=${TAG_VERSION/v/}
+TAG_VERSION=3.16.3
 
 
 # Publish the package
