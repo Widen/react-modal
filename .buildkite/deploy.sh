bbcc6c3837e69a10faef569a76f7add97322e6f6
Joseph Phelan <jphelan@widen.com>
2024-01-18 09:00:17 -0600

    sdfsdf

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Get the tag version from Buildkite or default to a pre-release tag
 TAG_VERSION=$(buildkite-agent meta-data get "tag-version" --default "0.0.0-alpha-$(date +%Y%m%d%H%M%S)")
-# TAG_VERSION=${TAG_VERSION/v/}
+TAG_VERSION=$(TAG_VERSION/v/)
 
 #setup git
 # git config user.email "Widen Developers"
