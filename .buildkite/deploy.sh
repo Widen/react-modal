d0e1edb2c9c264aaf82b1e7a49b0cb47ae6bead1
Joseph Phelan <jphelan@widen.com>
2024-01-18 10:26:31 -0600

    update version logic

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -3,16 +3,13 @@ set -euo pipefail
 
 # Get the tag version from Buildkite or default to a pre-release tag
 TAG_VERSION=$(buildkite-agent meta-data get "tag-version" --default "0.0.0-alpha-$(date +%Y%m%d%H%M%S)")
-# TAG_VERSION=${TAG_VERSION/v/}
-TAG_VERSION=3.16.3
-
+TAG_VERSION=${TAG_VERSION/v/}
 
 # Publish the package
 echo "versioning package ${TAG_VERSION}"
 npm version ${TAG_VERSION} --no-git-tag-version
 echo "publishing ${TAG_VERSION}"
 npm publish --tag tag-${TAG_VERSION}
-
 
 # Annotate the build with the types version
 buildkite-agent annotate --style success "Published to \`@widen/react-modal@${TAG_VERSION}\`. Install with the following command:
